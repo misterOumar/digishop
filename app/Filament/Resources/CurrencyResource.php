f9f0ac7ebfe2e0d431cb2616b4720fdc27b0dddb
Oumar <coulibalyoumartc@gmail.com>
2024-08-20 00:50:14 +0000

    add currencies to project

---
+++ app/Filament/Resources/CurrencyResource.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CurrencyResource\Pages;
+use App\Filament\Resources\CurrencyResource\RelationManagers;
+use App\Models\Currency;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Tapp\FilamentCountryCodeField\Forms\Components\CountryCodeSelect;
+use Symfony\Component\Intl\Countries;
+
+class CurrencyResource extends Resource
+{
+    protected static ?string $model = Currency::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-currency-dollar';
+
+    protected static ?string $navigationLabel = 'Monnaies';
+
+    protected static ?int $navigationSort = 5;
+
+    public static function form(Form $form): Form
+    {
+        $countries = config('countries');
+
+          // Inverser les clés et valeurs pour enregistrer le nom du pays directement
+    $invertedCountries = array_flip($countries);
+
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name'),
+                Forms\Components\TextInput::make('code'),
+                Forms\Components\TextInput::make('symbol'),
+                Forms\Components\Select::make('country')
+                ->label('Pays')
+                ->options($countries)
+                ->searchable()
+                ->required()
+                ->reactive()
+                ->afterStateUpdated(function ($state, callable $set) use ($countries) {
+                    $countryName = $countries[$state] ?? null;
+                    $set('country', $countryName); 
+                }),
+               
+
+                Forms\Components\Toggle::make('is_active')->default(true),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCurrencies::route('/'),
+            'create' => Pages\CreateCurrency::route('/create'),
+            'edit' => Pages\EditCurrency::route('/{record}/edit'),
+        ];
+    }
+}
